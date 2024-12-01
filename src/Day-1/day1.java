f26f1c147496fcbd239ca37942db8dc31ecc630d
Jashanjot Singh <jashansingh9890@gmail.com>
2024-12-01 23:48:15 +0530

    AOCday1part2

--- src/Day-1/day1.java
+++ src/Day-1/day1.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 
 //\\s: Matches any whitespace character. This includes spaces, tabs (\t), newlines (\n), and other whitespace.
@@ -12,6 +13,7 @@ class day1 {
         String filePath = "/home/jsb/Adventofcode/untitled/src/Day-1/inputd1.txt"; // Relative path from the project root
         ArrayList<Integer> leftlist = new ArrayList<Integer>();
         ArrayList<Integer> rightlist = new ArrayList<Integer>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         String line;
         while ((line = br.readLine()) != null) {
@@ -21,18 +23,28 @@ class day1 {
         }
         Collections.sort(leftlist);
         Collections.sort(rightlist);
-
+        //first-part
         int distance = 0;
         for(int i = 0 ; i < leftlist.size() ; i++) {
             distance += Math.abs(leftlist.get(i) - rightlist.get(i));
         }
-
+        for(int num : rightlist)
+        {
+            map.put(num,map.getOrDefault(num,0) + 1);
+        }
+        //second part
+        int result = 0;
+        for(int num : leftlist)
+        {
+            if(map.containsKey(num))
+            {
+                result += num * map.get(num);
+            }
+        }
+        System.out.println(result);
         System.out.println(distance);
-
-        //first-part done
-
         br.close();
 
-        System.out.println(leftlist);
+
     }
 }
